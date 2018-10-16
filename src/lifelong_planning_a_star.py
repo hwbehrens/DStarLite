5c7c12f2812d6433b2c103844f0adf02bb8a58a7
hwbehrens <hw@behrens.cc>
2018-10-15 17:04:12 -0700

    Initial commit
    I started implementing the original D* before I realized that was wrong.

---
+++ src/lifelong_planning_a_star.py
@@ -0,0 +1,10 @@
+# Contributors:
+# Hans Behrens
+# Barath Gunari
+# Rishabh Hatgadkar
+# Nicholas Martinez
+#
+# This implementation of LPA* based on the following paper:
+# "Improved fast replanning for robot navigation in unknown terrain"
+# https://doi.org/10.1109/ROBOT.2002.1013481
+#
