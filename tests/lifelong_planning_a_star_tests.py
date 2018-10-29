2ab2d2c9755be3237f83664879e4c2612121708d
hwbehrens <hw@behrens.cc>
2018-10-29 14:27:53 -0700

    Initial LPA* tests

---
+++ tests/lifelong_planning_a_star_tests.py
@@ -0,0 +1,37 @@
+# Contributors:
+# Hans Behrens
+# Barath Gunari
+# Rishabh Hatgadkar
+# Nicholas Martinez
+
+# Provides basic testing functionality for the DualPriorityQueue class
+
+import sys
+import math
+
+sys.path.append("/Users/hwbehren/JetBrains/PycharmProjects/DStarLite")
+
+from src import lifelong_planning_a_star as lpa
+
+# test manhattan distance
+assert lpa.l1_dist((5, 5), (5, 5)) == 0
+assert lpa.l1_dist((0, 0), (0, 0)) == 0
+assert lpa.l1_dist((0, 0), (5, 5)) == 10
+assert lpa.l1_dist((5, 5), (0, 0)) == 10
+
+# test euclidean distance
+assert lpa.l2_dist((5, 5), (5, 5)) == 0
+assert lpa.l2_dist((0, 0), (0, 0)) == 0
+assert lpa.l2_dist((0, 0), (5, 5)) == math.sqrt(50)
+assert lpa.l2_dist((5, 5), (0, 0)) == math.sqrt(50)
+
+# test map 1 (no internal walls)
+#   ####
+#   #S #
+#   # G#
+#   ####
+
+map1 = lpa.LPAStar(start_coord=(0, 0), goal_coord=(1, 1), resolution=(2, 2))
+
+assert (map1.compute_shortest_path() == [(0, 0), (0, 1), (1, 1)] or
+        map1.compute_shortest_path() == [(0, 0), (1, 0), (1, 1)])
