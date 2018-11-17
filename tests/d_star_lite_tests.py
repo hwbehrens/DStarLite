4cd685bee99d6ca27e0f23cecce1def87a7f4179
Hans Behrens <hw@behrens.cc>
2018-11-16 18:27:40 -0700

    Started expanding the tests for D* lite

--- tests/d_star_lite_tests.py
+++ tests/d_star_lite_tests.py
@@ -22,7 +22,8 @@ from src import d_star_lite as dsl
 map_a = dsl.DStarLite(start_coord=(0, 0), goal_coord=(1, 1), resolution=(2, 2))
 step1 = map_a.take_step()
 step2 = map_a.take_step()
-assert map_a.extract_path() == [(0, 0), (1, 0), (1, 1)]
+path = map_a.get_route()
+assert path == [(0, 0), (1, 0), (1, 1)]
 
 # test 2 (further, no walls)
 #   #####
@@ -40,14 +41,12 @@ assert map_b.extract_path() == [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)]
 #   # ..#
 #   #  G#
 #   #####
-map_b.make_wall_at((2, 0))
-assert map_b.extract_path() == [(0, 0), (1, 0), (1, 1), (2, 1), (2, 2)]
-
-# test 3b: calling the same extraction twice uses the cached version from the last call
-assert map_b.extract_path() == [(0, 0), (1, 0), (1, 1), (2, 1), (2, 2)]
-
-# test 3c: can't explicitly compute the path if it's been computed
-map_b.compute_shortest_path()  # will crash if it tries to execute (improper error checking)
+step1 = map_a.take_step()
+map_b.make_wall_at((0, 2))  # i suddenly see a wall in front of me!
+step2 = map_a.take_step()
+step3 = map_a.take_step()
+step4 = map_a.take_step()
+assert map_b.get_route() == [(0, 0), (1, 0), (1, 1), (2, 1), (2, 2)]
 
 # test 4: discover ANOTHER blocking wall, revise plan
 #   #####
@@ -55,6 +54,8 @@ map_b.compute_shortest_path()  # will crash if it tries to execute (improper err
 #   #.# #
 #   #..G#
 #   #####
+map_b = dsl.DStarLite(start_coord=(0, 0), goal_coord=(2, 2), resolution=(3, 3))
+# step1
 map_b.make_wall_at((1, 1))
 assert map_b.extract_path() == [(0, 0), (0, 1), (0, 2), (1, 2), (2, 2)]
 
