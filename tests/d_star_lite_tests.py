8d1d844a6bdf2b7fda7d310fc08a91e2f2358193
Hans Behrens <hw@behrens.cc>
2018-11-18 15:07:02 -0700

    Added a number of new/modified tests for D* lite

--- tests/d_star_lite_tests.py
+++ tests/d_star_lite_tests.py
@@ -15,49 +15,66 @@ from src import d_star_lite as dsl
 
 # test map 1 (no internal walls)
 #   ####
-#   #S.#    map A
-#   # G#
+#   #S #    map A
+#   #.G#
 #   ####
 
 map_a = dsl.DStarLite(start_coord=(0, 0), goal_coord=(1, 1), resolution=(2, 2))
-step1 = map_a.take_step()
-step2 = map_a.take_step()
+map_a.take_step()
+map_a.take_step()
 path = map_a.get_route()
 assert path == [(0, 0), (1, 0), (1, 1)]
 
 # test 2 (further, no walls)
 #   #####
-#   #S..#   map B
-#   #  .#
-#   #  G#
+#   #S  #   map B
+#   #.  #
+#   #..G#
 #   #####
 
 map_b = dsl.DStarLite(start_coord=(0, 0), goal_coord=(2, 2), resolution=(3, 3))
-assert map_b.extract_path() == [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)]
+assert map_b.extract_path() == [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)]  # expected path
+map_b.take_step()
+map_b.take_step()
+map_b.take_step()
+map_b.take_step()
+map_b.take_step()
+assert map_b.get_route() == [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)]  # actual path should match expected
 
 # test 3: discover a blocking wall, revise plan
 #   #####
-#   #S.##
-#   # ..#
-#   #  G#
+#   #S  #
+#   #.. #
+#   ##.G#
 #   #####
-step1 = map_a.take_step()
-map_b.make_wall_at((0, 2))  # i suddenly see a wall in front of me!
-step2 = map_a.take_step()
-step3 = map_a.take_step()
-step4 = map_a.take_step()
-assert map_b.get_route() == [(0, 0), (1, 0), (1, 1), (2, 1), (2, 2)]
+map_b = dsl.DStarLite(start_coord=(0, 0), goal_coord=(2, 2), resolution=(3, 3))
+map_b.take_step()
+map_b.make_wall_at((2, 0))  # i suddenly see a wall in front of me!
+assert map_b.extract_path() == [(1, 0), (1, 1), (2, 1), (2, 2)]  # expected path
+map_b.take_step()
+map_b.take_step()
+map_b.take_step()
+assert map_b.get_route() == [(0, 0), (1, 0), (1, 1), (2, 1), (2, 2)]  # actual (overall) path
 
 # test 4: discover ANOTHER blocking wall, revise plan
 #   #####
-#   #S ##
-#   #.# #
-#   #..G#
+#   #S..#
+#   #.#.#
+#   ## G#
 #   #####
 map_b = dsl.DStarLite(start_coord=(0, 0), goal_coord=(2, 2), resolution=(3, 3))
-# step1
-map_b.make_wall_at((1, 1))
-assert map_b.extract_path() == [(0, 0), (0, 1), (0, 2), (1, 2), (2, 2)]
+map_b.take_step()
+map_b.make_wall_at((1, 1))  # dead end, now what?
+map_b.make_wall_at((2, 0))
+path = map_b.extract_path()
+assert path == [(1, 0), (0, 0), (0, 1), (0, 2), (1, 2), (2, 2)]
+map_b.take_step()
+map_b.take_step()
+map_b.take_step()
+map_b.take_step()
+map_b.take_step()
+map_b.take_step()
+assert map_b.get_route() == [(0, 0), (1, 0), (0, 0), (0, 1), (0, 2), (1, 2), (2, 2)]
 
 # test 5: trivial case where goal == start
 map_c = dsl.DStarLite(start_coord=(0, 0), goal_coord=(0, 0), resolution=(10, 10))
@@ -70,6 +87,11 @@ assert map_c.extract_path() == [(0, 0)]
 map_d = dsl.DStarLite(start_coord=(0, 0), goal_coord=(2, 0), resolution=(3, 1))
 map_d.make_wall_at((1, 0))
 assert map_d.extract_path() is None
+map_d.take_step()  # can't move, should do nothing
+try:
+    map_d.make_wall_at((2, 0))
+except ValueError as e:
+    assert "non-adjacent" in str(e)  # cannot insert walls at non-adjacent locations
 
 # all done
 print("Testing complete!")
